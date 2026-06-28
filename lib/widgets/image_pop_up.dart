89d7a8eb7884be83613a14345d5893bdb9810457
Sudharsshan <sudharsshansy@gmail.com>
2026-06-28 15:25:47 +0530

    updated the pubspec and launch to support CORS origin and removed unwanted imports

--- lib/widgets/image_pop_up.dart
+++ lib/widgets/image_pop_up.dart
@@ -2,7 +2,6 @@ import 'dart:ui' as ui;
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:nfs_alloy/misllaneous/image_downloader.dart';
 import 'package:nfs_alloy/models/game_categories.dart';
 import 'package:nfs_alloy/models/wallpaper_loader.dart';
